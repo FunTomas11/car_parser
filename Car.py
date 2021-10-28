4a4b2039b15632bc72d79655b1de2de5fd726db5
FunTomas11 <krainievlc@gmail.com>
2021-10-28 22:22:08 +0200

    Refactored types

--- Car.py
+++ Car.py
@@ -1,9 +1,9 @@
-import CarType
+from CarType import CarType
 
 
 class Car:
 
-    def __init__(self, car_id, link, car_type: CarType = CarType.CarType.MASTER):
+    def __init__(self, car_id, link, car_type: CarType = CarType.MASTER):
         self.car_id = car_id
         self.link = link
         self.car_type = car_type
